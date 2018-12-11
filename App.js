ff219f64452754fdc4991655c1f1bf53d9a0d9a6
dl57934 <dl57934@pukyong.ac.kr>
2018-12-12 03:51:34 +0900

    first Design

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.upper}>
+          <Text style={styles.upperText}>Here Now's Time</Text>
+        </View>
+        <View style={styles.middle}>
+          <Text>here Alarm List</Text>
+        </View>
+        <View style={styles.down}>
+          <TouchableOpacity>
+            <MaterialIcons name="add-alarm" size={80} color="white" />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -14,8 +24,24 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#00008C"
   },
+  upper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40
+  },
+  down: {
+    flex: 1,
+    alignItems: "flex-end",
+    marginRight: 20,
+    marginBottom: 30
+  },
+  upperText: {
+    fontSize: 30
+  },
+  middle: {
+    flex: 5
+  }
 });
